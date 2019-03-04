4d32d87ed47009e97a8e418cfb6583dd99f62827
asony <asony@ncsu.edu>
2019-03-03 20:00:08 -0500

    Added error handling

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var util = require('./util.js');
 
 var target_file_path = process.argv[2];
-var files_to_consider = util.get_paths(target_file_path);
-merged_output = util.merge(files_to_consider);
+var files_to_consider;
+try {
+	files_to_consider = util.get_paths(target_file_path);
+} catch(error) {
+	process.stdout.write(error+"\n");
+  	process.exit();
+}
+var merged_output = util.merge(files_to_consider);
 
 process.stdout.write(merged_output);
